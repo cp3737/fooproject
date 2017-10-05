bf8ae2e08808e2860daf7c16bfb484a30ba09b6b
Sonya Mollinger <sonya.mollinger@gmail.com>
2017-10-05 18:50:17 -0400

    Test git push commit.

--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
-
+  
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
